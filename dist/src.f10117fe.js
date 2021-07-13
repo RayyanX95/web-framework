ac7ac5d2c99e1cc25186dfa0ebe9cd57e0990cb8
Ibarhim <ibrahim.alrayany@gmail.com>
2021-07-13 22:25:44 +0200

    Add Eventing class

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -1938,7 +1938,6 @@ var User =
 function () {
   function User(data) {
     this.data = data;
-    this.events = {};
   }
 
   ;
@@ -1949,24 +1948,6 @@ function () {
 
   User.prototype.set = function (update) {
     Object.assign(this.data, update);
-  };
-
-  User.prototype.on = function (eventName, callback) {
-    var handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  };
-
-  User.prototype.trigger = function (eventName) {
-    var handlers = this.events[eventName];
-
-    if (!handlers || !handlers.length) {
-      return;
-    }
-
-    handlers.forEach(function (callback) {
-      return callback();
-    });
   };
 
   User.prototype.fetch = function () {
